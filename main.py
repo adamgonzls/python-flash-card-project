51c581bf0d50597b3cd3f32aa3be65887fbf0ed5
Adam Gonzales <adamgonzales1@gmail.com>
2023-02-22 09:53:21 -0700

    feature: set up basic ui

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ from tkinter import *
 
 BACKGROUND_COLOR = "#B1DDC6"
 
+# functions
+
 
 # ui
 window = Tk()
 window.title("Flashy")
-window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+window.config(padx=30, pady=30, bg=BACKGROUND_COLOR)
+
+canvas = Canvas(height=526, width=800, bg="red")
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.grid(row=0, column=0, columnspan=2)
+
+wrong_image = PhotoImage(file="images/wrong.png")
+wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button.grid(row=1, column=0)
+
+right_image = PhotoImage(file="images/right.png")
+right_button = Button(image=right_image, highlightthickness=0)
+right_button.grid(row=1, column=1)
 
 window.mainloop()
